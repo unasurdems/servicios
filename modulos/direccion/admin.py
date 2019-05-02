5e30b8311bed972e666d8c575a4f077658a5d090
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2019-05-02 02:59:06 -0400

    adicion de tablas en el modulo de direccion

--- modulos/direccion/admin.py
+++ modulos/direccion/admin.py
@@ -7,7 +7,10 @@ from .models import (
     Departamento,
     Provincia,
     Alcaldia,
-    Municipio
+    Municipio,
+    Zona,
+    Marker,
+    Direccion
 )
 
 
@@ -41,4 +44,47 @@ class MunicipioAdmin(ModelLogAdmin):
     list_display = (
         'municipio',
         'municipio_descripcion'
+    )
+
+
+@admin.register(Zona)
+class ZonaAdmin(ModelLogAdmin):
+    list_display = (
+        'zonauv',
+        'zonauv_codigo_zona_completo',
+        'zonauv_codigo_zona',
+        'zonauv_descr',
+        'zonauv_cod_adm',
+        'municipio'
+    )
+
+
+@admin.register(Marker)
+class MarkerAdmin(ModelLogAdmin):
+    list_display = (
+        'marker',
+        'marker_id',
+        'marker_position',
+        'marker_lat',
+        'marker_long',
+        'marker_visible',
+        'marker_visible2',
+        'marker_drag',
+        'zona'
+    )
+
+
+@admin.register(Direccion)
+class DireccionAdmin(ModelLogAdmin):
+    list_display = (
+        'zona_barrio_uv_otro',
+        'calle_avenida',
+        'dir_referencial',
+        'numero',
+        'edificio',
+        'piso',
+        'departamento_local_oficina',
+        'longitud',
+        'latitud',
+        'servicio_id'
     )
--- modulos/direccion/models.py
+++ modulos/direccion/models.py
@@ -40,3 +40,44 @@ class Municipio(LogModel):
 
     def __str__(self):
         return self.municipio
+
+
+class Zona(LogModel):
+    zonauv = models.CharField(_('Zona Unidad Vecinal'), max_length=150, null=True, blank=True)
+    zonauv_codigo_zona_completo = models.CharField(_('Zona Codigo completo'), max_length=150, null=True, blank=True)
+    zonauv_codigo_zona = models.CharField(_('Zona codigo'), max_length=150, null=True, blank=True)
+    zonauv_descr = models.TextField(_('Zona UV descripcion'), null=True, blank=True)
+    zonauv_cod_adm = models.CharField(_('Zona Codigo Adm'), max_length=150, null=True, blank=True)
+    municipio = models.ForeignKey(Municipio, related_name='zona_municipio', on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.zonauv
+
+
+class Marker(LogModel):
+    marker = models.CharField(_('Marker'), max_length=150, null=True, blank=True)
+    marker_id = models.CharField(_('Marker ID'), max_length=15, null=True, blank=True)
+    marker_position = models.CharField(_('Marker Posicion'), max_length=150, null=True, blank=True)
+    marker_lat = models.CharField(_('Marker Latitud'), max_length=15, null=True, blank=True)
+    marker_long = models.CharField(_('Marker Longitud'), max_length=15, null=True, blank=True)
+    marker_visible = models.CharField(_('Marker Visible'), max_length=10, null=True, blank=True)
+    marker_visible2 = models.CharField(_('Marker Visible 2'), max_length=10, null=True, blank=True)
+    marker_drag = models.CharField(_('Marker Drag'), max_length=150, null=True, blank=True)
+    zona = models.ForeignKey(Zona, related_name='marker_zona', on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.marker
+
+
+class Direccion(LogModel):
+    zona_barrio_uv_otro = models.CharField(_('Zona Barrio UV Otro'), max_length=100, null=True, blank=True)
+    calle_avenida = models.CharField(_('Calle/Avenida'), max_length=100, null=True, blank=True)
+    dir_referencial = models.CharField(_('Dir. Referencial'), max_length=100, null=True, blank=True)
+    numero = models.CharField(_('Numero'), max_length=50, null=True, blank=True)
+    edificio = models.CharField(_('Edificio'), max_length=100, null=True, blank=True)
+    piso = models.CharField(_('Piso'), max_length=50, null=True, blank=True)
+    departamento_local_oficina = models.CharField(_('Departamento Local Oficina'), max_length=50, null=True, blank=True)
+    longitud = models.CharField(_('Longitud'), max_length=15, null=True, blank=True)
+    latitud = models.CharField(_('Latitud'), max_length=15, null=True, blank=True)
+    servicio_id = models.IntegerField(_('Servicio ID'), null=False, blank=False, unique=True)
+
